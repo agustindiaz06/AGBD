5e0c08284f179eae1de7daa84e98f527d20d584f
agustindiaz <diazagustiin30@gmail.com>
2024-03-19 15:58:03 -0300

    juego sql island

--- agustin_diaz.sql
+++ agustin_diaz.sql
@@ -1,3 +1,8 @@
 
 select * from inhabitant
 SELECT * FROM inhabitant WHERE state = "friendly"
+SELECT * FROM inhabitant WHERE state = "friendly" and job ="weaponsmith"
+SELECT * FROM inhabitant WHERE job LIKE '%smith' and state = "friendly"
+SELECT personid from inhabitant where name = "Stranger"
+SELECT GOLD from inhabitant where name = "Stranger"
+SELECT * from item WHERE owner IS NULL
